ce5987625fe411061447bcb638c4b6767cba1067
Eve Montalvao <eve.ionica@gmail.com>
2017-11-15 13:10:19 -0200

    Refactor: crawler

--- crawler.js
+++ crawler.js
@@ -1,11 +1,7 @@
 const cheerio = require('cheerio');
 const request = require('request');
-
-class Zaha {
-    constructor () {
-        let page = 2;
-        this.results = [];
-        this.singleRealState = {
+const fs = require('fs');
+let singleRealState = {
             source: {},
             characteristics: {
                 pricing: {}
@@ -20,16 +16,19 @@ class Zaha {
                 }
             }
         };
-
-        let urlSale = 'https://www.vivareal.com.br/venda/?__vt=prefetch:c&pagina=' + page;
-        let urlRent = 'https://www.vivareal.com.br/aluguel/?__vt=prefetch:c&pagina=' + page;
+class Zaha {
+    constructor () {
+        let page = 30;
 
         do {
             page = page + 1;
 
-            this.getBody(urlRent, this.getBasicInfo.bind(this));
-        } while (page <= 2);
-        
+            let urlSale = 'https://www.vivareal.com.br/venda/?__vt=prefetch:c&pagina=' + page;
+            let urlRent = 'https://www.vivareal.com.br/aluguel/?__vt=prefetch:c&pagina=' + page;
+            this.getBody(urlRent, this.getBasicInfo.bind(this)); 
+
+            console.log(page);       
+        } while (page < 40);
     }
 
     getBody (url, callback) {
@@ -55,25 +54,15 @@ class Zaha {
         const $ = cheerio.load(body, { decodeEntities: false });
 
         let realStates = $('.hbs-results-pages__property-card');
-        let limit = 1;
+        realStates = realStates.slice(0, 19);
 
-        realStates.map((index, item) => {          
-            this.singleRealState.characteristics.title = $(item).find('.js-card-title').html();
-            this.singleRealState.characteristics.description = $(item).find('.ko').html();
-            this.singleRealState.characteristics.area = parseInt($(item).find('.js-property-card-detail-area').html());
-            this.singleRealState.characteristics.images = $(item).find('.js-propertyCard').attr('data-images');
-            this.singleRealState.source.url = $(item).find('.js-card-title').attr('href');
-            this.singleRealState.characteristics.pricing.condominium = this.parseValues($(item).find('.js-condo-price').html());
+        realStates.map((index, item) => {   
+            singleRealState.source.url = $(item).find('.js-card-title').attr('href');
 
-            limit = limit + 1;
+            singleRealState.characteristics.title = $(item).find('.js-card-title').html();
+            singleRealState.characteristics.images = $('[data-images]').attr('data-images').replace(/[\[\]]/g, '').split(',').slice(0,5);
 
-            if(limit > 4) {
-                setTimeout(() => {
-                    this.singleRealState.location = this.getAddress($, item, '.js-property-card-address');
-                }, 5000);
-            };
-
-            this.getBody('https://www.vivareal.com.br/' + this.singleRealState.source.url, this.getAdvancedInfo.bind(this));
+            this.getBody('https://www.vivareal.com.br/' + singleRealState.source.url, this.getAdvancedInfo.bind(this));
 
         }, this);
 
@@ -83,27 +72,35 @@ class Zaha {
         if (!body) return;
 
         const $ = cheerio.load(body, { decodeEntities: false });
+        
+        this.getAddress($, '.js-title-location');
+        singleRealState.source.title = $('.js-title-main-info').html();
+        singleRealState.source.name = 'Viva Real';
+        singleRealState.source.code = $('.js-title-code').html();
+        singleRealState.characteristics.propertyType = $('.js-detail-type-value').html();
+        singleRealState.characteristics.categories = this.getCategories($, '.js-title-main-info');
+        singleRealState.characteristics.pricing.purchase = this.parseValues($('.js-detail-sale-price').html());
+        singleRealState.characteristics.pricing.rent = this.parseValues($('.js-detail-rent-price').html());
+        singleRealState.characteristics.pricing.tax = this.parseValues($('.js-detail-iptu-price').html());
+        singleRealState.characteristics.pricing.condominium = this.parseValues($('.js-detail-condo-price').html());
+        singleRealState.characteristics.area = parseInt($('.js-detail-area-value').html());
+        singleRealState.characteristics.description = $('.js-description-and-features > .bP').html().replace(/<\/?[^>]+(>|$)/g, "");
+        singleRealState.features.essencials.rooms = this.parseValues($('.js-detail-rooms').find('.bF').html());
+        singleRealState.features.essencials.suites = this.parseValues($('.js-detail-rooms').find('.bJ').html());
+        singleRealState.features.essencials.bathrooms = this.parseValues($('.js-detail-bathrooms').find('span').html());
+        singleRealState.features.essencials.kitchens = this.checkForEssencials($, 'cozinha');
+        singleRealState.features.essencials.serviceArea = this.checkForEssencials($, 'área de serviço');
+        singleRealState.features.essencials.parkingSpace = this.parseValues($('.js-detail-parking-spaces').find('.bF').html());
+        singleRealState.features.amenities.all.push($('.bS > li').html());
 
-        this.singleRealState.source.name = $('.creci').prev().html();
-        this.singleRealState.source.code = $('.js-title-code').html();
-        this.singleRealState.characteristics.propertyType = $('.js-detail-type-value').html();
-        this.singleRealState.characteristics.categories = this.getCategories($, '.js-title-main-info');
-        this.singleRealState.characteristics.pricing.purchase = this.parseValues($('.js-detail-sale-price').html());
-        this.singleRealState.characteristics.pricing.rent = this.parseValues($('.js-detail-rent-price').html());
-        this.singleRealState.characteristics.pricing.tax = this.parseValues($('.js-detail-iptu-price').html());
-        this.singleRealState.features.essencials.rooms = this.parseValues($('.js-detail-rooms').find('.bF').html());
-        this.singleRealState.features.essencials.suites = this.parseValues($('.js-detail-rooms').find('.bJ').html());
-        this.singleRealState.features.essencials.bathrooms = this.parseValues($('.js-detail-bathrooms').find('span').html());
-        this.singleRealState.features.essencials.kitchens = this.checkForEssencials($, 'cozinha');
-        this.singleRealState.features.essencials.serviceArea = this.checkForEssencials($, 'área de serviço');
-        this.singleRealState.features.essencials.parkingSpace = this.parseValues($('.js-detail-parking-spaces').find('.bF').html());
-        this.singleRealState.features.amenities.all.push($('.bS > li').html());
-
-        this.post();
     }
 
     checkForEssencials ($, query) {
-        let wrapper = $('.bS > li').html().toLowerCase();
+
+        let wrapper = $('.bS > li').html()
+        if(!wrapper) return false;
+
+        wrapper = wrapper.toLowerCase();
 
         if (wrapper == query) return true;
 
@@ -134,50 +131,50 @@ class Zaha {
         return type;
     }
 
-    getAddress ($, item, htmlClass) {
+    getAddress ($, htmlClass) {
         let addressObj = {
             geolocation: {}
         };
-        let fullAddress = $(item).find(htmlClass).find('span').html();
+        let fullAddress = $(htmlClass).html();
 
         let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + fullAddress + '&key=AIzaSyC-652MU3cZE-LlGdEf0_ZvS9JZ52eno8Q';
 
         request(url, (err, res, body) => {
             if (err || !body) throw err;
 
-            if (res.statusCode != 200) {
-                console.log('Status code: ' + res.statusCode);
-                return;
-            }
+            if (res.statusCode != 200) return;
 
             body = JSON.parse(body);
-            let addressComponents = body.address_components;
+            let result = body.results[0];
 
-            addressComponents.map(item => {
-                if(item.type.includes('postal_code')) addressObj.zipcode = item.long_name;
-                if(item.type.includes('route')) addressObj.street = item.long_name;
-                if(item.type.includes('street_number')) addressObj.number = item.long_name;
-                if(item.type.includes('sublocality')) addressObj.district = item.long_name;
-                if(item.type.includes('administrative_area_level_2')) addressObj.city = item.long_name;
-                if(item.type.includes('administrative_area_level_1')) addressObj.state = item.long_name;
-                if(item.type.includes('country')) addressObj.country = item.long_name;
-                addressObj.geolocation.latitude = JSON.parse(item.geometry.location.lat);
-                addressObj.geolocation.longitude = JSON.parse(item.geometry.location.lng);
+            result.address_components.map(item => {
+                if(item.types.includes('postal_code')) addressObj.zipcode = item.long_name;
+                if(item.types.includes('route')) addressObj.street = item.long_name;
+                if(item.types.includes('street_number')) addressObj.number = item.long_name;
+                if(item.types.includes('sublocality')) addressObj.district = item.long_name;
+                if(item.types.includes('administrative_area_level_2')) addressObj.city = item.long_name;
+                if(item.types.includes('administrative_area_level_1')) addressObj.state = item.long_name;
+                if(item.types.includes('country')) addressObj.country = item.long_name;
+                addressObj.geolocation.latitude = JSON.parse(result.geometry.location.lat);
+                addressObj.geolocation.longitude = JSON.parse(result.geometry.location.lng);
             });
 
-        })        
-        // console.log(addressObj);
-    }
+            singleRealState.location = addressObj;
+        }); 
+        this.post(singleRealState);
 
-    post () {
-        // request({
-        //     url: "https://niemeyer.hackaetano.com/properties",
-        //     method: "POST",
-        //     json: true, 
-        //     body: this.singleRealState,
-        // }, function (error, response, body){
-        //     console.log(response);
-        // }); 
+    };
+
+    post (data) {
+        request({
+            url: "https://niemeyer.hackaetano.com/properties/",
+            method: "POST",
+            json: true, 
+            body: data,
+        }, function (error, response, body){
+            console.log(error);
+            console.log(response);
+        }); 
     }
 }
 
